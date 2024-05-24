df48e28c5376504119622462be4886004d7a2b7e
gharvey <gato.harvey@defenseunicorns.com>
2024-05-23 17:14:11 -0700

    Replaces bigints with timestamps, renames usage from reserved word, create new type

--- supabase/migrations/20240522141100_threads_runs_messages_steps.sql
+++ supabase/migrations/20240522141100_threads_runs_messages_steps.sql
@@ -3,7 +3,7 @@ create table
   thread_objects (
     id uuid primary key DEFAULT uuid_generate_v4(),
     user_id uuid references auth.users not null,
-    created_at bigint default extract(epoch from now()) not null,
+    created_at timestamp without time zone DEFAULT NOW(),
     metadata jsonb,
   );
 
@@ -12,7 +12,7 @@ create table
   message_objects (
     id uuid primary key DEFAULT uuid_generate_v4(),
     user_id uuid references auth.users not null,
-    created_at bigint default extract(epoch from now()) not null,
+    created_at timestamp without time zone DEFAULT NOW(),
     thread_id uuid references thread_objects(id),
     role text,
     created_run_id references run_objects(run_id), -- The run_id the message was created by
@@ -25,25 +25,27 @@ create table
   message_file_objects (
     id uuid primary key DEFAULT uuid_generate_v4(),
     user_id uuid references auth.users not null,
-    created_at bigint default extract(epoch from now()) not null,
+    created_at timestamp without time zone DEFAULT NOW(),
     message_id uuid references message_objects(id),
     file_id uuid references file_objects(id)
   );
+
+CREATE TYPE run_status AS ENUM ('queued', 'in_progress', 'requires_action', 'cancelling', 'cancelled', 'failed', 'completed', 'incomplete', 'expired');
 
 -- Create a table to store the OpenAI Run Objects
 create table
   run_objects (
     run_id uuid primary key DEFAULT uuid_generate_v4(),
     user_id uuid references auth.users not null,
-    created_at bigint default extract(epoch from now()) not null,
+    created_at timestamp without time zone DEFAULT NOW(),
     assistant_id uuid references assistant_objects(id),
     thread_id uuid references thread_objects(id),
-    status text,
-    started_at bigint,
-    expires_at bigint,
-    cancelled_at bigint,
-    failed_at bigint,
-    completed_at bigint,
+    status run_status,
+    started_at timestamp without time zone,
+    expires_at timestamp without time zone,
+    cancelled_at timestamp without time zone,
+    failed_at timestamp without time zone,
+    completed_at timestamp without time zone,
     last_error jsonb,
     model text,
     instructions text,
@@ -52,7 +54,7 @@ create table
     file_ids uuid[],
     metadata jsonb,
     incomplete_details jsonb,
-    usage jsonb,
+    token_usage jsonb,
     temperature float,
     max_prompts_tokens int,
     max_completion_tokens int,
@@ -66,17 +68,17 @@ create table
   run_step_objects (
     step_id uuid primary key DEFAULT uuid_generate_v4(),
     object text,
-    created_at bigint default extract(epoch from now()) not null,
+    created_at timestamp without time zone DEFAULT NOW(),
     run_id uuid,
     assistant_id uuid,
     thread_id uuid,
     type text,
     status text,
-    cancelled_at bigint,
-    completed_at bigint,
-    expired_at bigint,
-    failed_at bigint,
+    cancelled_at timestamp without time zone,
+    completed_at timestamp without time zone,
+    expired_at timestamp without time zone,
+    failed_at timestamp without time zone,
     last_error jsonb,
     step_details jsonb,
-    usage jsonb
+    token_usage jsonb
   );
