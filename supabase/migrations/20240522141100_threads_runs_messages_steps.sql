ed079e44fd6913aad22a8853a005c530fd4d227b
gharvey <gato.harvey@defenseunicorns.com>
2024-05-29 15:31:34 -0700

    Returns schema to be back more in line the OpenAI objects

--- supabase/migrations/20240522141100_threads_runs_messages_steps.sql
+++ supabase/migrations/20240522141100_threads_runs_messages_steps.sql
@@ -3,7 +3,9 @@ create table
   thread_objects (
     id uuid primary key DEFAULT uuid_generate_v4(),
     user_id uuid references auth.users not null,
+    object text check (object in ('thread')),
     created_at timestamp without time zone DEFAULT NOW(),
+    tool_resources jsonb,
     metadata jsonb,
   );
 
@@ -12,22 +14,18 @@ create table
   message_objects (
     id uuid primary key DEFAULT uuid_generate_v4(),
     user_id uuid references auth.users not null,
+    object text check (object in ('thread.message')),
     created_at timestamp without time zone DEFAULT NOW(),
     thread_id uuid references thread_objects(id),
+    status text,
+    completed_at timestamp without time zone,
+    incomplete_at timestamp without time zone,
     role text,
-    created_run_id references run_objects(run_id), -- The run_id the message was created by
     content jsonb,
+    assistant_id uuid references assistant_objects(id),
+    run_id uuid references run_objects(run_id),
+    attachments jsonb,
     metadata jsonb
-  );
-
--- Create a table to store the OpenAI Message File Objects
-create table
-  message_file_objects (
-    id uuid primary key DEFAULT uuid_generate_v4(),
-    user_id uuid references auth.users not null,
-    created_at timestamp without time zone DEFAULT NOW(),
-    message_id uuid references message_objects(id),
-    file_id uuid references file_objects(id)
   );
 
 CREATE TYPE run_status AS ENUM ('queued', 'in_progress', 'requires_action', 'cancelling', 'cancelled', 'failed', 'completed', 'incomplete', 'expired');
@@ -35,49 +33,56 @@ CREATE TYPE run_status AS ENUM ('queued', 'in_progress', 'requires_action', 'can
 -- Create a table to store the OpenAI Run Objects
 create table
   run_objects (
-    run_id uuid primary key DEFAULT uuid_generate_v4(),
+    id uuid primary key DEFAULT uuid_generate_v4(),
     user_id uuid references auth.users not null,
+    object text check (object in ('thread.run')),
     created_at timestamp without time zone DEFAULT NOW(),
-    assistant_id uuid references assistant_objects(id),
     thread_id uuid references thread_objects(id),
+    assistant_id uuid references assistant_objects(id),
     status run_status,
-    started_at timestamp without time zone,
+    required_action jsonb,
+    last_error jsonb,
     expires_at timestamp without time zone,
+    started_at timestamp without time zone,
     cancelled_at timestamp without time zone,
     failed_at timestamp without time zone,
     completed_at timestamp without time zone,
-    last_error jsonb,
+    incomplete_details jsonb,
     model text,
     instructions text,
-    stream boolean,
-    tools text[],
-    file_ids uuid[],
+    tools jsonb,
     metadata jsonb,
+    stream boolean,
+    file_ids uuid[],
     incomplete_details jsonb,
     token_usage jsonb,
     temperature float,
+    top_p int,
     max_prompts_tokens int,
     max_completion_tokens int,
     truncation_strategy jsonb,
-    response_format jsonb,
     tool_choice jsonb
+    response_format jsonb,
   );
 
 -- Create a table to store the OpenAI Run Step Objects
 create table
   run_step_objects (
-    step_id uuid primary key DEFAULT uuid_generate_v4(),
+    id uuid primary key DEFAULT uuid_generate_v4(),
+    object text check (object in ('thread.run.step')),
     created_at timestamp without time zone DEFAULT NOW(),
-    run_id uuid references run_objects(run_id),
+    assistant_id uuid references assistant_objects(id),
     thread_id uuid references thread_objects(id),
+    run_id uuid references run_objects(run_id),
     step_type text,
     status text,
-    cancelled_at timestamp without time zone,
-    completed_at timestamp without time zone,
-    expired_at timestamp without time zone,
-    failed_at timestamp without time zone,
-    last_error jsonb,
     step_details jsonb,
+    last_error jsonb,
+    expired_at timestamp without time zone,
+    cancelled_at timestamp without time zone,
+    failed_at timestamp without time zone,
+    completed_at timestamp without time zone,
+    metadata jsonb,
     token_usage jsonb
   );
 
@@ -115,7 +120,7 @@ CREATE INDEX run_step_thread_id ON run_step (thread_id);
 -- Indexes for foreign keys for message_objects
 CREATE INDEX message_objects_user_id ON message_objects (user_id);
 CREATE INDEX message_objects_thread_id ON message_objects (thread_id);
-CREATE INDEX message_objects_created_run_id ON message_objects (created_run_id);
+CREATE INDEX message_objects_created_run_id ON message_objects (run_id);
 
 -- Indexes for foreign keys for thread_objects
 CREATE INDEX thread_objects_user_id ON thread_objects (user_id);
