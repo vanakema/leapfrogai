6fb11daad81e70febe9504c2847255394d93fb44
gharvey <gato.harvey@defenseunicorns.com>
2024-05-29 11:15:52 -0700

    Removes circular dependency

--- src/leapfrogai_api/backend/types.py
+++ src/leapfrogai_api/backend/types.py
@@ -1,6 +1,8 @@
 """Typing definitions for assistants API."""
 
 from __future__ import annotations
+
+import datetime
 from typing import Literal
 from pydantic import BaseModel, Field
 from fastapi import UploadFile, Form, File
@@ -9,8 +11,6 @@ from openai.types import FileObject
 from openai.types.beta import VectorStore
 from openai.types.beta import Assistant, AssistantTool
 from openai.types.beta.assistant import ToolResources
-
-from leapfrogai_api.backend.helpers import add_days_to_timestamp
 
 
 ##########
@@ -292,6 +292,29 @@ class CreateVectorStoreRequest(BaseModel):
     expires_after: ExpiresAfter | None = ExpiresAfter(anchor="last_active_at", days=0)
     metadata: dict | None = {}
 
+    def add_days_to_timestamp(self, timestamp: int, days: int) -> int:
+        """
+        Adds a specified number of days to a timestamp. Used to when updating the VectorStore.
+
+        Args:
+            timestamp(int): An integer representing a timestamp.
+            days(int): The number of days to add.
+
+        Returns:
+            An integer representing the new timestamp with the added days.
+        """
+
+        # Convert the timestamp to a datetime object
+        datetime_obj = datetime.datetime.fromtimestamp(timestamp)
+
+        # Add the specified number of days
+        new_datetime_obj = datetime_obj + datetime.timedelta(days=days)
+
+        # Convert the new datetime object back to a timestamp
+        new_timestamp = new_datetime_obj.timestamp()
+
+        return int(new_timestamp)
+
     def can_expire(self) -> bool:
         return self.expires_after is not None
 
@@ -306,7 +329,7 @@ class CreateVectorStoreRequest(BaseModel):
             A tuple of when the vector store should expire and the timestamp of the expiry date.
         """
         if self.can_expire():
-            return self.expires_after, add_days_to_timestamp(
+            return self.expires_after, self.add_days_to_timestamp(
                 last_active_at, self.expires_after.days
             )
         else:
