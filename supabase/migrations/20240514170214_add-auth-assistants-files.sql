fcaa6506dc876b456ba9b52572ba41a6ae3b2bfc
Gregory Horvath <gphorvath@defenseunicorns.com>
2024-06-03 13:14:29 -0400

    update migration

--- supabase/migrations/20240514170214_add-auth-assistants-files.sql
+++ supabase/migrations/20240514170214_add-auth-assistants-files.sql
@@ -3,3 +3,7 @@ alter table assistant_objects
 
 alter table file_objects
         add column user_id uuid references auth.users not null;
+
+update storage.buckets
+    set public = false
+    where id = 'file_bucket';
