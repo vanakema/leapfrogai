0f89d0e697c2764b49db4fe79eaa61f7eec0f226
gharvey <gato.harvey@defenseunicorns.com>
2024-06-04 16:07:38 -0700

    Removes unnecessary file

--- tests/pytest/leapfrogai_api/fiddling.py
+++
@@ -1,7 +0,0 @@
-from openai.types.beta.threads.run_create_params import RunCreateParamsBase
-
-def test_do_a_thing():
-    request: dict = { "assistant_id": "stuff"}
-    request_params: RunCreateParamsBase = RunCreateParamsBase(**request)
-
-    print(request_params)
